443210d1cc940545457653b9e64ab9e3d7040294
Dev Pratap Singh Chauhan <devpratapsinghchauhan523@gmail.com>
2026-02-26 14:32:03 +0530

    Update TodoList.js

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -11,7 +11,7 @@ export default function TodoList() {
 
   const fetchTodos = async () => {
     try {
-      const res = await API.get("/v1/getTodo", { withCredentials: true });
+      const res = await API.get("/v1/getTodo");
       setTodos(res.data.data);
     } catch (err) {
       console.error(err);
